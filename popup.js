55a67b0c095aad5784082b880d13412bbe0abc87
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-02-03 22:32:03 +0530

    final commit

--- popup.js
+++ popup.js
@@ -1,61 +1,92 @@
 const btn = document.querySelector('.highlight-text');
 const btn2 = document.querySelector('.download-text');
 
-
-// this functions checks if something is highlighted and if there it, it takes it and saves it in the local storage
 btn.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-    // var texts = JSON.parse(localStorage.getItem("selectedText"));
-    // console.log("this is the text before", texts);
-
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
         function: highlightText,
-    }, async (injectionResluts) => {
-        const [data] = injectionResluts;
-        console.log(data.result);
-
-        if (localStorage.getItem('selectedText') === null) {
-            localStorage.setItem('selectedText', JSON.stringify(data.result));
-            window.alert("Text has been saved");
-        }
-        else {
-            var texts = JSON.parse(localStorage.getItem("selectedText"));
-            texts.push(data.result[0]);
-            console.log(texts);
-            localStorage.setItem('selectedText', JSON.stringify(texts));
-            window.alert("Text has been saved");
-        }
     })
 })
 
-
-// this function checks if there is any selected text and if there is any,it returns the data
+// function that stores selected texts in the local storage and highlights the selected text
 function highlightText() {
     try {
+        // getting the selected text
         let highlightedText = window.getSelection().toString();
         if (highlightedText) {
+            highlightedText = highlightedText + "\n\n";
             const note = [];
             note.push(highlightedText);
-            console.log(note);
-            return note;
+            //storing the text in the local storage
+            if (localStorage.getItem('selectedText') === null) {
+                localStorage.setItem('selectedText', JSON.stringify(note));
+            }
+            else {
+                var texts = JSON.parse(localStorage.getItem("selectedText"));
+                texts = [...texts, highlightedText]
+                localStorage.setItem('selectedText', JSON.stringify(texts));
+                window.alert("Text has been saved");
+            }
+
+            // highlights the selected text
+            var selectedText = window.getSelection().toString();
+            if (selectedText) {
+                var newNode = document.createElement("mark");
+                newNode.appendChild(document.createTextNode(selectedText));
+                window.getSelection().getRangeAt(0).surroundContents(newNode);
+            }
         }
+
+        // no text is highlighted
         else { window.alert("Nothing is selected") }
     } catch (error) {
         console.error(error);
     }
 }
 
+
+
 // fucntion to download all the selected lines in a .txt fiels
 btn2.addEventListener('click', async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: downloadText,
+    })
+
+})
+
+// fucntion to download all the selected lines in a .txt fiels
+function downloadText() {
     const selected_texts = JSON.parse(localStorage.getItem('selectedText'));
-    console.log(selected_texts);
     const link = document.createElement('a');
     const file = new Blob(selected_texts, { type: 'text/plain' });
     link.href = URL.createObjectURL(file);
     link.download = "selected_texts.txt";
     link.click();
     URL.revokeObjectURL(link.href);
-    localStorage.clear();
-})
+
+
+    console.log("csv")
+    // saving data in xls format
+    var ws_name = "seletctedText";//"SheetJS";
+    var wb = new Workbook(), ws = sheet_from_array_of_arrays(selected_texts);
+    /* add worksheet to workbook */
+    wb.SheetNames.push(ws_name);
+    wb.Sheets[ws_name] = ws;
+    var wbout = XLSX.write(wb, { bookType: 'xlsx', bookSST: true, type: 'binary' });
+    saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), filename + ".xlsx")
+}
+
+
+
+
+// var mySentence = "Software is a set of computer programs";
+// var allText = document.body.textContent;
+// var index = allText.search(mySentence);
+// if (index !== -1) {
+//     console.log("The sentence '" + mySentence + "' was found in the web page at index " + index);
+// } else {
+//     console.log("The sentence '" + mySentence + "' was not found in the web page.");
+// }
